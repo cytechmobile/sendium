d7471828e8f5fcac96b83a2c398db8b6f9191413
pavlos <pavlos@cytech.gr>
2026-06-08 12:43:28 +0300

    test(converter): add Playwright e2e coverage
    Signed-off-by: pavlos <pavlos@cytech.gr>

--- sendium-kannel-converter/src/components/SummaryMetrics.vue
+++ sendium-kannel-converter/src/components/SummaryMetrics.vue
@@ -11,25 +11,25 @@ defineProps({
   <v-row class="metrics-row">
     <v-col cols="6" md="3">
       <v-card class="metric-card" elevation="0">
-        <span>Credentials</span>
+        <span>Credentials : </span>
         <strong>{{ summary.credentials }}</strong>
       </v-card>
     </v-col>
     <v-col cols="6" md="3">
       <v-card class="metric-card" elevation="0">
-        <span>SMPP clients</span>
+        <span>SMPP clients : </span>
         <strong>{{ summary.smppClients }}</strong>
       </v-card>
     </v-col>
     <v-col cols="6" md="3">
       <v-card class="metric-card" elevation="0">
-        <span>SMPP servers</span>
+        <span>SMPP servers : </span>
         <strong>{{ summary.smppServers }}</strong>
       </v-card>
     </v-col>
     <v-col cols="6" md="3">
       <v-card class="metric-card" elevation="0">
-        <span>Warnings/errors</span>
+        <span>Warnings/errors : </span>
         <strong>{{ summary.warnings }}/{{ summary.errors }}</strong>
       </v-card>
     </v-col>
