37fe458619f0065c9b6327410fbcb5640848763b
pavlos <pavlos@cytech.gr>
2026-08-20 17:39:49 +0300

    fix(test): harden native metric polling
    Signed-off-by: pavlos <pavlos@cytech.gr>

--- sendium-core/src/main/java/gr/cytech/sendium/core/worker/Tracker.java
+++ sendium-core/src/main/java/gr/cytech/sendium/core/worker/Tracker.java
@@ -12,12 +12,18 @@ public interface Tracker<M extends StandardMessage> {
 
     void configure(String key, String newValue, String oldValue);
 
+    /**
+     * @param hashedProviderMessageId precomputed hash retained for tracker implementations that use hashed indexes
+     */
     int updateSendStatusAndExtID(String hashedProviderMessageId, M message, String providerMessageId);
 
     String getHashedMessageID(String messageId);
 
     String getVendorPriceGateway();
 
+    /**
+     * @param hashedProviderMessageId precomputed hash retained for tracker implementations that use hashed indexes
+     */
     void createAndEnqueueDLR(int mqid, String providerMessageId, String hashedProviderMessageId,
                              String from, String to, String body, int state, String errorCode,
                              HashMap<String, String> tlvs);
